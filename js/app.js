b67983c6f6219917d1e98d251489fd34572b362c
Ye lin Htun <yelinhtun1987@gmail.com>
2015-09-06 22:54:50 +0800

    added history table and update the data.js file

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var app = angular.module("main", [
     "exchangeFactory",
-    "ngCookies"
+    "ngCookies",
+    "oitozero.ngSweetAlert"
 ]);
 
 app.config(['$httpProvider', function($httpProvider) {
@@ -9,7 +10,8 @@ app.config(['$httpProvider', function($httpProvider) {
     }
 ]);
 
-app.controller("mainController", ["$scope", "$cookies", "Exchange", function ($scope, $cookies, Exchange) {
+app.controller("mainController", ["$scope", "$cookies", "Exchange", "SweetAlert",
+        function ($scope, $cookies, Exchange, SweetAlert) {
 
     $scope.model = [];
     $scope.model.items = itemType;
@@ -24,9 +26,16 @@ app.controller("mainController", ["$scope", "$cookies", "Exchange", function ($s
     $scope.itemPrice = 0.0;
     $scope.dataPriceService = 0;
     $scope.invalid = false;
-
     $scope.history = [];
+    $scope.historyTotalPrice = 0;
+    $scope.historyItem = {
+        item : "",
+        type : "",
+        quantity : 0,
+        total : 0
+    };
 
+    // get the exchange rate api
     if(! $cookies.get("exchangeRate")) {
         Exchange.get()
             .then(function(returnData) {
@@ -43,13 +52,18 @@ app.controller("mainController", ["$scope", "$cookies", "Exchange", function ($s
         $scope.exchangeRate = rounding($cookies.get("exchangeRate"));
     }
 
-
     // change event for Item dropdown
     $scope.dataItemChange = function() {
         $scope.model.types = [];
         for(var i =0; i < $scope.model.items.length; i++) {
             var item = $scope.model.items[i];
+
+            $scope.historyItem.item = "";
+
             if(item.value == $scope.dataItem) {
+
+                $scope.historyItem.item = item.name;
+
                 $scope.model.types = item.type;
                 $scope.dataType = "0";
                 return;
@@ -62,7 +76,13 @@ app.controller("mainController", ["$scope", "$cookies", "Exchange", function ($s
         $scope.model.dataQuantity = [];
         for(var i =0; i < $scope.model.types.length; i++) {
             var type = $scope.model.types[i];
+
+            $scope.historyItem.type = ""
+
             if(type.value == $scope.dataType) {
+
+                $scope.historyItem.type = type.name;
+
                 $scope.model.quantity = type.price;
                 $scope.dataQuantity = "0";
                 return;
@@ -74,10 +94,18 @@ app.controller("mainController", ["$scope", "$cookies", "Exchange", function ($s
     $scope.dataQuantityChange = function() {
         for(var i = 0; i < $scope.model.quantity.length; i++) {
             var qty = $scope.model.quantity[i];
+
+            $scope.historyItem.quantity = 0;
+
             if(qty.q == $scope.dataQuantity) {
                 $scope.dataPriceDelivery = qty.p;
+
+                $scope.historyItem.quantity = qty.q;
                 calculateTotal();
                 return;
+            }
+            else {
+                $scope.dataTotal = 0.0
             }
         }
     }
@@ -91,13 +119,62 @@ app.controller("mainController", ["$scope", "$cookies", "Exchange", function ($s
         $scope.dataQuantityChange();
     }
 
+    $scope.saveItem = function() {
+        if($scope.itemPrice <= 0) {
+            SweetAlert.swal("Item Price cannot be 0."); return;
+        }
+        else if($scope.historyItem.item == "") {
+            SweetAlert.swal("Please select the Item."); return;
+        }
+        else if($scope.historyItem.type == "") {
+            SweetAlert.swal("Please select the Type."); return;
+        }
+        else if($scope.historyItem.quantity <= 0) {
+            SweetAlert.swal("Quantity cannot be 0."); return;
+        }
+
+        var local = angular.copy($scope.historyItem);
+        $scope.history.push(local);
+        calculateTotalHistoryPrice();
+    }
+
+    function calculateTotalHistoryPrice() {
+        $scope.historyTotalPrice = 0;
+        for(var i = 0; i < $scope.history.length; i++) {
+            $scope.historyTotalPrice += $scope.history[i].total;
+        }
+    }
+
+    $scope.clearItem = function() {
+        SweetAlert.swal({
+           title: "Are you sure?",
+           text: "Your will not be able to recover this history list!",
+           type: "warning",
+           showCancelButton: true,
+           confirmButtonColor: "#DD6B55",confirmButtonText: "Yes, delete it!",
+           cancelButtonText: "No, cancel plx!",
+           closeOnConfirm: false,
+           closeOnCancel: false },
+        function(isConfirm){
+           if (isConfirm) {
+                $scope.history = [];
+                SweetAlert.swal("Deleted!", "Your imaginary file has been deleted.", "success");
+           } else {
+              SweetAlert.swal("Cancelled", "Your history item is safe :)", "error");
+           }
+        });
+
+    }
+
     function calculateTotal(deliveryPrice) {
         var totalItemPrice = $scope.itemPrice * $scope.dataQuantity;
-        console.log('total item price : ' + totalItemPrice);
+
         $scope.dataPriceService = rounding( ($scope.dataPriceDelivery + totalItemPrice) * 0.1 );
 
         $scope.dataTotal = ($scope.dataPriceService + totalItemPrice + $scope.dataPriceDelivery) * $scope.exchangeRate;
         $scope.dataTotal = rounding($scope.dataTotal);
+
+        $scope.historyItem.total = $scope.dataTotal;
     }
 
     function rounding(number) {
--- js/data.js
+++ js/data.js
@@ -6,17 +6,17 @@ var itemType = [
             {
                 "name": "Dress",
                 "value": "dress",
-                "price": [{"q":1, "p":6}, {"q":2, "p":10}, {"q":3, "p":14}, {"q":4, "p":18}, {"q":5, "p":22}]
+                "price": [{"q":1, "p":7}, {"q":2, "p":11}, {"q":3, "p":15}, {"q":4, "p":19}, {"q":5, "p":23}]
             },
             {
                 "name": "Top",
                 "value": "top",
-                "price": [{"q":1, "p":5}, {"q":2, "p":9}, {"q":3, "p":12}, {"q":4, "p":15}, {"q":5, "p":18}]
+                "price": [{"q":1, "p":6}, {"q":2, "p":10}, {"q":3, "p":13}, {"q":4, "p":16}, {"q":5, "p":19}]
             },
             {
                 "name": "Thin Coat",
                 "value": "thin_coat",
-                "price": [{"q":1, "p":6}, {"q":2, "p":10}, {"q":3, "p":13}, {"q":4, "p":16}, {"q":5, "p":19}]
+                "price": [{"q":1, "p":7}, {"q":2, "p":11}, {"q":3, "p":14}, {"q":4, "p":17}, {"q":5, "p":20}]
             },
             {
                 "name": "Thick Coat",
@@ -32,17 +32,17 @@ var itemType = [
             {
                 "name": "Flat Shoes, Slipper, Flat Sandal",
                 "value": "slipper",
-                "price": [{"q":1, "p":5}, {"q":2, "p":9}, {"q":3, "p":12}, {"q":4, "p":14}, {"q":5, "p":16}]
+                "price": [{"q":1, "p":6}, {"q":2, "p":10}, {"q":3, "p":13}, {"q":4, "p":15}, {"q":5, "p":17}]
             },
             {
                 "name": "Shoes with Medium Heel",
                 "value": "medium_heel",
-                "price": [{"q":1, "p":6}, {"q":2, "p":11}, {"q":3, "p":15}, {"q":4, "p":18}, {"q":5, "p":22}]
+                "price": [{"q":1, "p":7}, {"q":2, "p":12}, {"q":3, "p":16}, {"q":4, "p":19}, {"q":5, "p":23}]
             },
             {
                 "name": "Shoes with High Heel",
                 "value": "high_heel",
-                "price": [{"q":1, "p":7}, {"q":2, "p":12}, {"q":3, "p":16}, {"q":4, "p":19}, {"q":5, "p":22}]
+                "price": [{"q":1, "p":8}, {"q":2, "p":13}, {"q":3, "p":17}, {"q":4, "p":20}, {"q":5, "p":23}]
             },
              {
                 "name": "Sneakers and Wedges Shoes",
@@ -84,7 +84,7 @@ var itemType = [
             {
                 "name": "Watch / Bracelet",
                 "value": "watch_bracelet",
-                "price": [{"q":1, "p":2}, {"q":2, "p":3}, {"q":3, "p":4}, {"q":4, "p":5}, {"q":5, "p":5}]
+                "price": [{"q":1, "p":4}, {"q":2, "p":5}, {"q":3, "p":6}, {"q":4, "p":6.5}, {"q":5, "p":7}]
             },
             {
                 "name": "Necklace",
@@ -94,7 +94,7 @@ var itemType = [
             {
                 "name": "Sunglasses",
                 "value": "sunglasses",
-                "price": [{"q":1, "p":3}, {"q":2, "p":5}, {"q":3, "p":6}, {"q":4, "p":7.5}, {"q":5, "p":8.5}]
+                "price": [{"q":1, "p":4}, {"q":2, "p":5}, {"q":3, "p":6}, {"q":4, "p":7.5}, {"q":5, "p":8.5}]
             }
         ]
     }
